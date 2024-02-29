6a15849f4dc44aa62439132eaef6ee31b0b23df6
MPS Vihanha <>
2024-02-29 20:03:31 +0530

    Nav links Navigations Smoothing

--- script.js
+++ script.js
@@ -39,3 +39,25 @@ btnScrollTo.addEventListener('click', () => {
     behavior: 'smooth',
   });
 });
+
+//Nav bar links Smooth Scrolling
+document.querySelectorAll('.nav__link').forEach(element => {
+  element.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+//1. Add event listener to common parent element
+//2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
