c88f56560ce16e48d636980e2fb4b4cd2844c951
MPS Vihanha <>
2024-02-29 12:34:03 +0530

    smooth scrolling

--- script.js
+++ script.js
@@ -29,3 +29,13 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//Smooth Scrolling to a section
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', () => {
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
